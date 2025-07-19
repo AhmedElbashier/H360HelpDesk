3f650ade41fc83ca237a1d62f0038a31fa97794d
Ahmed Elbashier <ahmedelbashier.22@gmail.com>
2025-07-19 22:29:59 +0400

    license controller

--- LicenseGeneratorApi/Controllers/LicenseController.cs
+++ LicenseGeneratorApi/Controllers/LicenseController.cs
@@ -1,43 +1,7 @@
-<<<<<<< HEAD
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.IO;
-//using System.Text;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Cryptography;
-//using Microsoft.Net.Http.Headers;
-//using LicenseGeneratorApi.Models;
-
-//namespace LicenseGeneratorApi.Controllers
-//{
-//    [Route("api/v1/[controller]")]
-//    [ApiController]
-//    public class LicenseControllerTest : ControllerBase
-//    {
-//        [HttpPost]
-//        public IActionResult GenerateLicense([FromBody] LicenseRequest request)
-//        {
-//            // Validate the request (e.g., user limit)
-//            if (request.AdminsLimit <= 0)
-//            {
-//                return BadRequest("User limit must be greater than zero.");
-//            }
-//            if (request.AdminsLimit <= 0)
-//            {
-//                return BadRequest("User limit must be greater than zero.");
-//            }
-//            // Generate a unique license key (you can use any logic you prefer)
-//            string licenseKey = Guid.NewGuid().ToString("N");
-
-//            // Generate a unique license key
-//            string licenseKey = Guid.NewGuid().ToString("N");
-=======
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 using System.Text;
-using System.Security.Cryptography;
 using Microsoft.Net.Http.Headers;
 using LicenseGeneratorApi.Models;
 
@@ -59,10 +23,8 @@ namespace LicenseGeneratorApi.Controllers
             {
                 return BadRequest("User limit must be greater than zero.");
             }
-            // Generate a unique license key
+            // Generate a unique license key (you can use any logic you prefer)
             string licenseKey = Guid.NewGuid().ToString("N");
-
-            string hardwareId = ComputeHardwareId(request.MacAddress ?? request.Secret);
 
             // Create a license object
             var license = new License
@@ -74,38 +36,8 @@ namespace LicenseGeneratorApi.Controllers
                 AgentsLimit = request.AgentsLimit,
                 SupervisorsLimit = request.SupervisorsLimit,
                 BackOfficeLimit = request.BackOfficeLimit,
-                ExpirationDate = DateTime.UtcNow.AddYears(1), // License expires in 1 year
-                HardwareId = hardwareId
+                ExpirationDate = DateTime.UtcNow.AddYears(1) // Example: License expires in 1 year
             };
->>>>>>> 211db96df5eda10efa565b70558f7e67b96850ac
-
-//            string hardwareId = ComputeHardwareId(request.MacAddress ?? request.Secret);
-
-//            // Create a license object
-//            var license = new License
-//            {
-//                Key = licenseKey,
-//                Company = request.Company,
-//                Vendor = request.Vendor,
-//                AdminsLimit = request.AdminsLimit,
-//                AgentsLimit = request.AgentsLimit,
-//                SupervisorsLimit = request.SupervisorsLimit,
-//                BackOfficeLimit = request.BackOfficeLimit,
-//                ExpirationDate = DateTime.UtcNow.AddYears(1) // Example: License expires in 1 year
-//                ExpirationDate = DateTime.UtcNow.AddYears(1), // License expires in 1 year
-//                HardwareId = hardwareId
-//            };
-
-//            // Serialize the license object to a .lic file content
-//            string licenseContent = SerializeLicense(license);
-
-<<<<<<< HEAD
-//            // Set the response headers for a file download
-//            var contentDisposition = new ContentDispositionHeaderValue("attachment")
-//            {
-//                FileName = "mylicense.lic"
-//            };
-//            Response.Headers.Add(HeaderNames.ContentDisposition, contentDisposition.ToString());
 
 //            // Return the license content as a file
 //            return File(Encoding.UTF8.GetBytes(licenseContent), "application/octet-stream");
@@ -129,22 +61,13 @@ namespace LicenseGeneratorApi.Controllers
 //        public int SupervisorsLimit { get; set; }
 //        public int BackOfficeLimit { get; set; }
 //    }
-=======
+
         private string SerializeLicense(License license)
         {
             // Serialize the license object to a string (you can use any serialization method)
             // Here, we are using JSON serialization for simplicity
             string serializedLicense = Newtonsoft.Json.JsonConvert.SerializeObject(license);
             return serializedLicense;
-        }
-
-        private string ComputeHardwareId(string? source)
-        {
-            if (string.IsNullOrEmpty(source))
-                return string.Empty;
-            using var sha = SHA256.Create();
-            var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(source));
-            return Convert.ToBase64String(hash);
         }
     }
 
@@ -156,21 +79,7 @@ namespace LicenseGeneratorApi.Controllers
         public int AgentsLimit { get; set; }
         public int SupervisorsLimit { get; set; }
         public int BackOfficeLimit { get; set; }
-        public string? MacAddress { get; set; }
-        public string? Secret { get; set; }
     }
->>>>>>> 211db96df5eda10efa565b70558f7e67b96850ac
-
-
-//        private string ComputeHardwareId(string? source)
-//        {
-//            if (string.IsNullOrEmpty(source))
-//                return string.Empty;
-//            using var sha = SHA256.Create();
-//            var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(source));
-//            return Convert.ToBase64String(hash);
-//        }
-//    }
 
 
 //}
