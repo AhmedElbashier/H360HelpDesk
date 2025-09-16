a4e2dda118e62dbc513082a9434700047b07e4a7
Ahmed Elbashier <62715278+AhmedElbashier@users.noreply.github.com>
2025-09-16 16:10:09 +0400

    api url update

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api.railway.app/api/v1/hdusers', // Replace with your Railway API URL
+  apiUrl: 'https://h360-helpdesk-api-production.up.railway.app/api/v1', // Replace with your Railway API URL
 };
